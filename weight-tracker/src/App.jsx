05a1f44cf2878115602f0c42ec55146a4699a442
LizzieDworsky <amydworsky15@gmail.com>
2022-06-14 08:17:50 -0600

    created the AddEntryForm child component and connected it to the app.jsx file

--- weight-tracker/src/App.jsx
+++ weight-tracker/src/App.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
 import DisplayEntries from "./Components/DisplayEntries";
+import AddEntry from "./Components/AddEntryForm";
 
 function App() {
     const [entries, setEntries] = useState([
-        { weight: 175, date: "11-23-2021" },
+        { weight: 175, date: "2022-05-23" },
     ]);
+
+    function addNewEntry(entry) {
+        let tempEntries = [...entries, entry];
+        setEntries(tempEntries);
+    }
 
     return (
         <div>
             <DisplayEntries entries={entries} />
+            <AddEntry addNewEntry={addNewEntry} />
         </div>
     );
 }
